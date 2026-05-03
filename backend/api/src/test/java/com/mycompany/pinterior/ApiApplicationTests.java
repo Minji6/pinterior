2e663e5c139740df94ca916686690622985ef527
Minji6 <105301353+Minji6@users.noreply.github.com>
2026-05-04 02:21:27 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ backend/api/src/test/java/com/mycompany/pinterior/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.pinterior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
