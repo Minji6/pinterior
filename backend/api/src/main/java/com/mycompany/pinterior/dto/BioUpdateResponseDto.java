067cf913d72e529db78dfd29a9ee2cb4bbcf8a92
Minji6 <105301353+Minji6@users.noreply.github.com>
2026-05-08 18:11:10 +0900

    [feat] 소개 수정 응답 DTO 추가

---
+++ backend/api/src/main/java/com/mycompany/pinterior/dto/BioUpdateResponseDto.java
@@ -0,0 +1,13 @@
+package com.mycompany.pinterior.dto;
+
+public class BioUpdateResponseDto {
+	private String bio;
+
+	public BioUpdateResponseDto(String bio) {
+		this.bio = bio;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+}
